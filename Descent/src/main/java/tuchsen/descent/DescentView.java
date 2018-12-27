17bbf04db2f0770d7acc9567228427b66f19ea5d
Tobias Baumhöver <tobi@localhost.localdomain>
2018-12-27 08:51:03 +0100

    fixed build, started controller support

--- Descent/src/main/java/tuchsen/descent/DescentView.java
+++ Descent/src/main/java/tuchsen/descent/DescentView.java
@@ -6,6 +6,7 @@ import android.graphics.Point;
 import android.os.Build;
 import android.os.Handler;
 import android.view.Display;
+import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -114,27 +115,141 @@ public class DescentView extends SurfaceView implements KeyEvent.Callback,  Surf
 
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_DEL) {
-			keyHandler((char) 0x0E, false);
-		} else if (keyCode == KeyEvent.KEYCODE_ENTER) {
-			keyHandler((char) 0x1C, false);
-		} else {
-			keyHandler((char) event.getUnicodeChar(), false);
-		}
+		char result = getCharForKeyCode(keyCode, event);
+
+		keyHandler(result, false);
+
 		return event.getUnicodeChar() != 0;
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_DEL) {
-			keyHandler((char) 0x0E, true);
-		} else if (keyCode == KeyEvent.KEYCODE_ENTER) {
-			keyHandler((char) 0x1C, true);
-		} else {
-			keyHandler((char) event.getUnicodeChar(), true);
-		}
+
+		char result = getCharForKeyCode(keyCode, event);
+
+		keyHandler(result, true);
+
 		return event.getUnicodeChar() != 0;
 	}
+
+	private static boolean isFireKey(int keyCode) {
+		// Here we treat Button_A and DPAD_CENTER as the primary action
+		// keys for the game.
+		return keyCode == KeyEvent.KEYCODE_BUTTON_A;
+	}
+
+	private char getCharForKeyCode(int keyCode, KeyEvent event) {
+		if ((event.getSource() & InputDevice.SOURCE_GAMEPAD)
+				== InputDevice.SOURCE_GAMEPAD) {
+			if (event.getRepeatCount() == 0) {
+				switch (keyCode) {
+					default:
+						if (isFireKey(keyCode)) {
+							return (char) 0x1d;
+							// Update the ship object to fire lasers
+						}
+						break;
+				}
+			}
+		}
+			else
+			{
+				if (keyCode == KeyEvent.KEYCODE_DEL) {
+					return (char) 0x0E;
+				} else if (keyCode == KeyEvent.KEYCODE_ENTER) {
+					return (char) 0x1C;
+				} else {
+					return (char) event.getUnicodeChar();
+				}
+			}
+
+		return (char) event.getUnicodeChar();
+	}
+
+	@Override
+	public boolean onGenericMotionEvent(MotionEvent event) {
+
+		// Check that the event came from a game controller
+		if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) ==
+				InputDevice.SOURCE_JOYSTICK &&
+				event.getAction() == MotionEvent.ACTION_MOVE) {
+
+			// Process all historical movement samples in the batch
+			final int historySize = event.getHistorySize();
+
+			// Process the movements starting from the
+			// earliest historical position in the batch
+			for (int i = 0; i < historySize; i++) {
+				// Process the event at historical position i
+				processJoystickInput(event, i);
+			}
+
+			// Process the current movement sample in the batch (position -1)
+			processJoystickInput(event, -1);
+			return true;
+		}
+		return super.onGenericMotionEvent(event);
+	}
+
+
+	private static float getCenteredAxis(MotionEvent event,
+										 InputDevice device, int axis, int historyPos) {
+		final InputDevice.MotionRange range =
+				device.getMotionRange(axis, event.getSource());
+
+		// A joystick at rest does not always report an absolute position of
+		// (0,0). Use the getFlat() method to determine the range of values
+		// bounding the joystick axis center.
+		if (range != null) {
+			final float flat = range.getFlat();
+			final float value =
+					historyPos < 0 ? event.getAxisValue(axis):
+							event.getHistoricalAxisValue(axis, historyPos);
+
+			// Ignore axis values that are within the 'flat' region of the
+			// joystick axis center.
+			if (Math.abs(value) > flat) {
+				return value;
+			}
+		}
+		return 0;
+	}
+
+    private void processJoystickInput(MotionEvent event, int historyPos) {
+
+        InputDevice mInputDevice = event.getDevice();
+
+        // Calculate the horizontal distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat axis, or the right control stick.
+        float x = getCenteredAxis(event, mInputDevice,
+                MotionEvent.AXIS_X, historyPos);
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_HAT_X, historyPos);
+        }
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_Z, historyPos);
+        }
+
+        // Calculate the vertical distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat switch, or the right control stick.
+        float y = getCenteredAxis(event, mInputDevice,
+                MotionEvent.AXIS_Y, historyPos);
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_HAT_Y, historyPos);
+        }
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_RZ, historyPos);
+        }
+
+        // Update the ship object based on the new x and y values
+    }
+
 
 	@Override
 	public boolean onKeyPreIme(int keyCode, KeyEvent event) {
@@ -180,7 +295,7 @@ public class DescentView extends SurfaceView implements KeyEvent.Callback,  Surf
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+		surfaceDestroyed(holder);
 	}
 
 	@Override
